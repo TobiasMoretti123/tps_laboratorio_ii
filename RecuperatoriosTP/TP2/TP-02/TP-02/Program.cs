a3019b46dc8247c63a6fde13553a76e6f04112b2
TobiasMoretti123 <78236685+TobiasMoretti123@users.noreply.github.com>
2022-07-29 15:52:10 -0300

    Resuelto

--- RecuperatoriosTP/TP2/TP-02/TP-02/Program.cs
+++ RecuperatoriosTP/TP2/TP-02/TP-02/Program.cs
@@ -47,7 +47,7 @@ namespace TP_02_2018
 
             // Quito 2 items y muestro
             taller -= c1;
-            taller -= new Ciclomotor(Vehiculo.EMarca.Honda, "ASD913", ConsoleColor.Red);
+            taller -= c2;
 
             Console.WriteLine(taller.ToString());
             Console.WriteLine("<-----------PRESIONE UNA TECLA PARA CONTINUAR----------->");
